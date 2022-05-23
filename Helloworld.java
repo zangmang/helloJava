eb263f7824139908272033697b73549db0845b76
zanmang <106054222+zangmang@users.noreply.github.com>
2022-05-23 15:14:42 +0900

    add.print
    print 기능추가

--- Helloworld.java
+++ Helloworld.java
@@ -2,4 +2,8 @@ public class Helloworld {
   publiv static void main(String[] srgs) {
     System.out.println("Hello, world");
   }
+  public static void main (String[] srgs) {
+    System.out.println("Hello, world");
+    Print("Hello, world");
+    System.out.println("Hello, world");
 }  
